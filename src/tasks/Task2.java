dcf7e158aca1df4c3128a0c0be2166f98986e310
AdrianOrd93 <adrian.ordak93@gmaill.com>
2018-12-28 15:18:22 +0100

    Udemy first and second tasks

---
+++ src/tasks/Task2.java
@@ -0,0 +1,17 @@
+package tasks;
+
+public class Task2 {
+    public static void main(String[] args) {
+        int[] tab = new int[3];
+        tab[0] = 2;
+        tab[1] = 4;
+        tab[2] = 7;
+
+        int sum  = tab[0] + tab[1] + tab[2];
+        int multiplication = tab[0] * tab[1] * tab[2];
+
+        System.out.println("Dodawanie tablic: " + sum);
+        System.out.println("----------------------------");
+        System.out.println("Mnożenie tablic: " + multiplication );
+    }
+}
